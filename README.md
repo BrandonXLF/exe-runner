cbd75b1d4f812869a8438539a271d71d266f680c
BrandonXLF <42590338+BrandonXLF@users.noreply.github.com>
2020-10-20 00:37:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# exe Runner
+
+**exe Runner** is an Visual Studio Code extension that allows you to easily run `.exe` files.
+
+## Features
+
+* Run `.exe` files by clicking button right of the tab bar
+* Run `.exe` files from the file explorer context menu for `.exe` files
+* Run `.exe` files from the editor context menu for `.exe` files
+* Use the command `Run Executable` to run `.exe` files
+
+## Example
+
+![example](media/example.gif)
---
+++ src/extension.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+import * as url from 'url';
+import { basename } from 'path';
+
+let terminal: vscode.Terminal | undefined;
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('exe-runner.run', (fileUri: vscode.Uri) => {
+		let path : string;
+		vscode.window.onDidCloseTerminal(() => terminal = undefined);
+		try {
+			path = url.fileURLToPath(fileUri.toString());
+		} catch (e) {
+			vscode.window.showErrorMessage(`Failed to run .exe file. ${e.message}.`);
+			return;
+		}
+		if (!path.endsWith('.exe')) {
+			vscode.window.showErrorMessage(`${basename(path)} is not an .exe file.`);
+			return;
+		}
+		terminal = terminal || vscode.window.createTerminal('exe Runner');
+		terminal.show();
+		terminal.sendText(`\"${path}\"`);
+	}));
+}
