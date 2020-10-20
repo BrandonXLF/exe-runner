89730b185ef4039e88e480e5ee6301d30a47156c
BrandonXLF <42590338+BrandonXLF@users.noreply.github.com>
2020-10-20 00:53:11 -0400

    Removed icon.icon, added .gitattributes, and cleaned up code

--- src/extension.ts
+++ src/extension.ts
@@ -6,20 +6,24 @@ let terminal: vscode.Terminal | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('exe-runner.run', (fileUri: vscode.Uri) => {
-		let path : string;
-		vscode.window.onDidCloseTerminal(() => terminal = undefined);
+		let path: string;
+
 		try {
 			path = url.fileURLToPath(fileUri.toString());
 		} catch (e) {
 			vscode.window.showErrorMessage(`Failed to run .exe file. ${e.message}.`);
 			return;
 		}
+
 		if (!path.endsWith('.exe')) {
 			vscode.window.showErrorMessage(`${basename(path)} is not an .exe file.`);
 			return;
 		}
+
 		terminal = terminal || vscode.window.createTerminal('exe Runner');
 		terminal.show();
 		terminal.sendText(`\"${path}\"`);
+
+		vscode.window.onDidCloseTerminal(() => terminal = undefined);
 	}));
 }
