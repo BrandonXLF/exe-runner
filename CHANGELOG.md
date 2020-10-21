54d3185b8c7b8d07885b69e1dbe4ceaa7aac91db
BrandonXLF <42590338+BrandonXLF@users.noreply.github.com>
2020-10-20 22:57:25 -0400

    Use cmd.exe in windows, wine on others

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Change Log
 
+## 0.1.0
+
+- Terminal always uses cmd.exe on windows
+- On non-Windows OSes, wine is used
+
 ## 0.0.2
 
 - Updated LICENSE filename
--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,29 @@
 import * as vscode from 'vscode';
-import * as url from 'url';
+import { fileURLToPath } from 'url';
 import { basename } from 'path';
 
 let terminal: vscode.Terminal | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('exe-runner.run', (fileUri: vscode.Uri) => {
-		let path: string;
+		let filePath: string;
+		let isWin = process.platform === 'win32';
 
 		try {
-			path = url.fileURLToPath(fileUri.toString());
+			filePath = fileURLToPath(fileUri.toString());
 		} catch (e) {
 			vscode.window.showErrorMessage(`Failed to run .exe file. ${e.message}.`);
 			return;
 		}
 
-		if (!path.endsWith('.exe')) {
-			vscode.window.showErrorMessage(`${basename(path)} is not an .exe file.`);
+		if (!filePath.endsWith('.exe')) {
+			vscode.window.showErrorMessage(`${basename(filePath)} is not an .exe file.`);
 			return;
 		}
 
-		terminal = terminal || vscode.window.createTerminal('exe Runner');
+		terminal = terminal || vscode.window.createTerminal('exe Runner', isWin ? 'C:\\Windows\\System32\\cmd.exe' : undefined);
 		terminal.show();
-		terminal.sendText(`\"${path}\"`);
+		terminal.sendText(`${isWin ? '' : 'wine '}"${filePath}"`);
 
 		vscode.window.onDidCloseTerminal(() => terminal = undefined);
 	}));
